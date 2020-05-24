ff4f78ad1a7a94e2b330ba6585eb46854f9b7f89
spv <aquaticvegetable@gmail.com>
2020-05-24 15:12:02 -0400

    Create lifesupportd.c
    had this lying around, might as well put it up in case it's useful.

---
+++ experimental/lifesupportd.c
@@ -0,0 +1,38 @@
+/*
+    lifesupportd
+    theoretically functional
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    printf("[*] init lifesupportd\n");
+    if (getuid() != 0) {
+        printf("[!] error. this daemon must be run as root.\n");
+        return -1;
+    }
+    FILE *fp;
+    fp = fopen("/.ranlifesupport", "rw");
+    fclose(fp);
+    printf("[*] prep done. sleeping...\n");
+    sleep(60);
+    while (1) {
+        if (access("/.stopuntether", F_OK) != -1) {
+            printf("[*] stopped! disabling, then sleeping.\n");
+            if (fork() == 0)
+                execl("/bin/bash", "/bin/bash", "-c", "dajb", NULL);
+            sleep(30);
+            if (fork() == 0)
+                execl("/bin/rm", "/bin/rm", "/.stopuntether", NULL);
+        }
+        else {
+            printf("[*] enabling...\n");
+            execl("/bin/bash", "/bin/bash", "-c", "enjb", NULL);
+            printf("[*] done\n");
+        }
+        sleep(10);
+    }
+    return 0;
+}
